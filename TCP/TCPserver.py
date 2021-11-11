596101ec1ca35326ac2720e0930a5ba0b534c2be
lisn0 <lisnoxvel@gmail.com>
2021-11-11 08:58:44 +0100

    3.2.1.13

--- TCP/TCPserver.py
+++ TCP/TCPserver.py
@@ -8,5 +8,9 @@ address = (HOST, PORT)
 
 tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_server.bind(address)
-tcp_server.listen()
+tcp_server.listen(0)
+
+while True:
+    conn, address_client = tcp_server.accept()
+    print('Accepted connection from {}:{}'.format(address_client[0], address_client[1]))
 
